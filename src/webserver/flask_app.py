3de6b18b7c076f37a27561e67b213617e20615b8
nlecluse <nicolas.lecluse@gmail.com>
2020-08-12 10:13:33 +0200

    remove print

--- src/webserver/flask_app.py
+++ src/webserver/flask_app.py
@@ -101,7 +101,6 @@ class FlaskThread(QThread):
 
     def run(self):
         asset_manager = AssetManager.getInstance()
-        print("started socketio")
         socket_io.run(flask_app, port=asset_manager.config('webapp', 'port'), host='0.0.0.0')
 
     def init_controller(self, controller_param):
