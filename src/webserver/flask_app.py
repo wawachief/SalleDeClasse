9c87827bbf275ef49e59e0527dee7ecfbafd28a8
Thomas.Lecluse <th.lecluse@gmail.com>
2020-08-13 10:47:15 +0200

    hotfix: now properly terminates python when quits application

--- src/webserver/flask_app.py
+++ src/webserver/flask_app.py
@@ -53,8 +53,7 @@ def handle_disconnect():
 @socket_io.on('stop-server')
 def stop_server():
     controller.flask_server.stop_flask()
-    # socket_io.stop()
-    os.kill(os.getpid(), signal.SIGABRT)
+    os.kill(os.getpid(), signal.SIGKILL)
 
 
 @socket_io.on('confirm_connect')
